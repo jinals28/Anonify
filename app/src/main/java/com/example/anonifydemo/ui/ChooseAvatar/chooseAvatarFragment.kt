4cb6445c743a7259607c82342191c17e3fa0dd63
Suman <sumansuthar182@gmail.com>
2024-02-26 13:45:42 +0530

    image assets,feed,manifest,gradle,post

--- app/src/main/java/com/example/anonifydemo/ui/ChooseAvatar/chooseAvatarFragment.kt
+++ app/src/main/java/com/example/anonifydemo/ui/ChooseAvatar/chooseAvatarFragment.kt
@@ -6,27 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.anonifydemo.R
+import com.example.anonifydemo.databinding.FragmentChooseAvatarBinding
+import com.example.anonifydemo.databinding.FragmentLoginBinding
 
-class chooseAvatarFragment : Fragment() {
+    class chooseAvatarFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = chooseAvatarFragment()
-    }
+    private var _binding : FragmentChooseAvatarBinding? = null
 
-    private lateinit var viewModel: ChooseAvatarViewModel
+    private val binding get() = _binding
 
+    //private lateinit var viewModel: ChooseAvatarViewModel
+    private lateinit var btnavatar: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choose_avatar, container, false)
-    }
+        _binding = FragmentChooseAvatarBinding.inflate(layoutInflater, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ChooseAvatarViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        return binding!!.root    }
 
-}
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            btnavatar = binding!!.btnavatar
+            btnavatar.setOnClickListener{
+                if(findNavController().currentDestination!!.id==R.id.chooseAvatarFragment){
+                    findNavController().navigate(R.id.action_chooseAvatarFragment_to_chooseTopic)
+
+                }
+            }
+        }
+    }
---
+++ app/src/main/java/com/example/anonifydemo/ui/home/PostViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.anonifydemo.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class PostViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
