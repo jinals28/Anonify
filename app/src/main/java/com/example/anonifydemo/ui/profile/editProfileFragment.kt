db0404af80bf7eea60d3b34cabda2216fd55f0fd
Jinal Shah <jainjinal7168@gmail.com>
2024-04-11 16:28:25 +0530

    Final Commit

--- app/src/main/java/com/example/anonifydemo/ui/profile/editProfileFragment.kt
+++ app/src/main/java/com/example/anonifydemo/ui/profile/editProfileFragment.kt
@@ -59,6 +59,7 @@ class editProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
        // editemail = binding!!.txteditemail
+
         editbio = binding!!.txteditbio
        // lblemail = binding!!.lblemail
 
